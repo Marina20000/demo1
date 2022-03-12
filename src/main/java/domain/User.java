41bdb17f731cd26667a9bb3b41fa42355817a8f5
Marina_Voronetckaia <jana-v@list.ru>
2022-03-12 13:49:37 +0400

    builder annotation

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,43 +1,14 @@
 package domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Builder
+@Getter
+@Setter
 public class User extends Dictionary{
     private String name;
     private String surname;
     private String lastname;
-
-    private User(Builder builder) {
-        this.name=builder.name;
-        this.surname=builder.surname;
-        this.lastname = builder.lastname;
-    }
-
-    public static class Builder{
-        private String name;
-        private String surname;
-        private String lastname;
-        private String version;
-        private Long id;
-
-        public Builder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Builder surname(String surname) {
-            this.surname = surname;
-            return this;
-        }
-
-        public Builder lastname(String lastname) {
-            this.lastname = lastname;
-            return this;
-        }
-
-        public User build() {
-            User user =  new User(this);
-            return user;
-        }
-
-    }
 }
